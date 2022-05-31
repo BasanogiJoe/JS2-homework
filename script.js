b546ad8b1f6eb8af0594fb4ab2cab0c2050f6250
Georgii <georgiigrigorchukwork@gmail.com>
2022-05-31 22:05:36 +0200

    Задание 2

--- script.js
+++ script.js
@@ -5,16 +5,39 @@ const goods = [
   { title: 'Shoes', price: 250 },
 ];
 
-const renderGoodsItem = ({ title = '', price = 0 }) => `
-      <div class="goods-item">
-        <h3>${title}</h3>
-        <p>${price}</p>
-      </div>
-      `;
-
-const renderGoodsList = (list = []) => {
-  let goodsList = list.map(item => renderGoodsItem(item)).join('');
-  document.querySelector('.goods-list').innerHTML = goodsList;
+class GoodsItem {
+  constructor({ title, price }) {
+    this.title = title;
+    this.price = price;
+  }
+  render() {
+    return `
+    <div class="goods-item">
+    <h3>${this.title}</h3>
+    <p>${this.price}</p>
+  </div>
+  `;
+  }
 }
 
-renderGoodsList(goods);
+class GoodsList {
+  items = [];
+  fetchGoods() {
+    this.items = goods;
+  }
+  calculatePrice() {
+    return this.items.reduce((prev, { price }) => prev + price, 0)
+  }
+
+  render() {
+    const goods = this.items.map(item => {
+      const goodsItem = new GoodsItem(item);
+      return goodsItem.render()
+    }).join('');
+    document.querySelector('.goods-list').innerHTML = goods;
+  }
+}
+
+const goodsList = new GoodsList();
+goodsList.fetchGoods();
+goodsList.render();
